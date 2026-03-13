b56a2c2a55fc548e98360f6a8f4b14fae21378d6
Daan0112 <daan.grupping@gmail.com>
2026-03-13 23:38:13 +0100

    Changed model to add MPEC/SLEC, change parameters, and add gaussian pulse.

--- ABM_model_Active.py
+++ ABM_model_Active.py
@@ -1,76 +1,60 @@
 import mesa
 import random
+import math
 
-def antigen(model):
-    return model.steps < model.T_antigen
+def gaussian_pulse(model):
+    return model.alpha_peak * math.exp( -0.5 * (( model.steps - model.t_peak ) / model.sigma)**2)
 
 class CD4Cell(mesa.Agent):
-    def __init__(self, model, cell_type = 'N', age = 0, path = "Undefined"):
+    def __init__(self, model, cell_type = 'Naive'):
         super().__init__(model)
-        self.cell_type = cell_type  # N, S, C, R <-- Naive, TSCM, TCM, TEMRA
-        self.age = age
-
-        #### Select path on initialization. ####
-        if path == "Undefined":
-            self.path = self.select_path()
-        else:
-            self.path = path
-
-    ####################################
-    ## ## ## # Helper methods # ## ## ##
-    ####################################
-
-    def select_path(self):  # Function to define path
-        if random.random() < self.model.frac_memory:
-            if random.random() < self.model.S_C_Ratio:
-                return "S"
-            else:
-                return "C"
-        else:
-            return "R"
-
-    ####################################
-    ## ## ## ## Main methods ## ## ## ##
-    ####################################
+        self.cell_type = cell_type  # Naive, TSCM, TCM, TEMRA, TEM SLEC, MPEC 
 
     # Activate T-cell is detected
     def activate(self):
+        random_value = random.random()
         # N to S/R
-        if self.cell_type == "N" and antigen(self.model) and random.random() < self.model.p_act:
-            self.cell_type = self.path
-            self.activation_time = self.model.steps
-        elif self.cell_type == "S" and random.random() < self.model.p_SC:
-            self.cell_type = "C"
-        elif self.cell_type == "C" and random.random() < self.model.p_CR:
-            self.cell_type = "R"
-
-    def proliferate(self):
-        if antigen(self.model):    
-            if self.cell_type == "N" and random.random() < self.model.r_N:
-                CD4Cell(self.model, cell_type = 'N')
-            elif self.cell_type == "S" and random.random() < self.model.r_S:
-                CD4Cell(self.model, cell_type = 'S')
-            elif self.cell_type == "C" and random.random() < self.model.r_C:
-                CD4Cell(self.model, cell_type = 'C')
-            elif self.cell_type == "R" and random.random() < self.model.r_R:
-                CD4Cell(self.model, cell_type = 'R')
+        if self.cell_type == "Naive" and random_value < gaussian_pulse(self.model):
+            if random.random() < self.model.q:
+                #SLEC Route
+                self.cell_type = "SLEC"
+                for _ in range(2**self.model.b_SLEC - 1):
+                    CD4Cell(self.model, cell_type = "SLEC")
+            else:
+                #MPEC Route
+                self.cell_type = "MPEC"
+                for _ in range(2**self.model.b_MPEC - 1):
+                    CD4Cell(self.model, cell_type = "MPEC")
+        
+        # Add mpec route
+        elif self.cell_type == "MPEC" and random_value < self.model.f_TSCM:
+            self.cell_type = "TSCM"
+        # The random value - f_TSCM is to not influence chances due to cells already changed to TSCM.
+        elif self.cell_type == "MPEC" and random_value - self.model.f_TSCM < self.model.f_TCM:
+            self.cell_type = "TCM"
+        
+        # Add Slec route
+        elif self.cell_type == "SLEC" and random_value < self.model.f_TEMRA:
+            self.cell_type = "TEMRA"
+        # The random value - f_TSCM is to not influence chances due to cells already changed to TSCM.
+        elif self.cell_type == "SLEC" and random_value - self.model.f_TSCM < self.model.f_TEM:
+            self.cell_type = "TEM"
 
     # Age the T-cell and handle transitions or death
     def death_age(self):
-        self.age += 1
         # Death based on cell type
-        if self.cell_type == "N" and random.random() < self.model.d_N:
+        if self.cell_type == "Naive" and random.random() < self.model.mu_N:
             self.remove()
-            self.death_time = self.model.steps
-        elif self.cell_type == "S" and random.random() < self.model.d_S:
+        elif self.cell_type == "TSCM" and random.random() < self.model.mu_TSCM:
             self.remove()
-            self.death_time = self.model.steps
-        elif self.cell_type == "C" and random.random() < self.model.d_C:
+        elif self.cell_type == "TCM" and random.random() < self.model.mu_TCM:
             self.remove()
-            self.death_time = self.model.steps
-        elif self.cell_type == "R" and random.random() < self.model.d_R:
+        elif self.cell_type == "TEMRA" and random.random() < self.model.mu_TEMRA:
             self.remove()
-            self.death_time = self.model.steps
+        elif self.cell_type == "SLEC" and random.random() < self.model.mu_SLEC:
+            self.remove()
+        elif self.cell_type == "MPEC" and random.random() < self.model.mu_MPEC:
+            self.remove()
 
     ###################################
     ## ## ## ## Step method ## ## ## ##
@@ -78,7 +62,6 @@ class CD4Cell(mesa.Agent):
 
     def step(self):
         self.activate()
-        self.proliferate()
         self.death_age()
 
 class Immunology_Model(mesa.Model):
@@ -86,61 +69,59 @@ class Immunology_Model(mesa.Model):
     Manager class to run Model
     """
     def __init__(self,
-                 T_antigen = 23,
-                 p_act=0.00022,
-                 frac_memory=0.75,
-                 S_C_Ratio=0.6,
-                 p_SC=0.0015,
-                 p_CR=0.0015,
-                 r_N=0.02,
-                 r_S=0.075,
-                 r_C=0.0,
-                 r_R=0.0,
-                 d_N=0.0005,
-                 d_S=0.0001,
-                 d_C=0.0016,
-                 d_R=0.011,
-                 initial_population=5_000,
-                 # global_factor=1
+                 mu_N = 0.0003,     # FIXED
+                 mu_TSCM = 0.0002,  # FIXED
+                 mu_TCM = 0.004,    # FIXED
+                 mu_TEM = 0.01,     # FIXED
+                 mu_TEMRA = 0.02,   # FIXED
+                 mu_MPEC = 0.02,    # FIXED
+                 mu_SLEC = 0.05,    # FIXED
+                 f_TSCM = 0.03,     # FIXED
+                 f_TCM = 0.05,      # FIXED
+                 f_TEM = 0.06,      # FIXED
+                 f_TEMRA = 0.02,    # FIXED
+                 t_peak = 18,       # FIXED
+                 sigma = 7,         # FIXED
+                 q = 0.35,          # FIXED
+                 alpha_peak = 0.01, # FREE [0.01-0.3]
+                 b_MPEC = 1,        # FREE [1-5]
+                 K_mem = 50,        # FREE [50-500]
+                 S_CD4=500,         # FREE [500-10000]
                  ):
         super().__init__()
-        # Time antigen present
-        self.T_antigen = int(T_antigen)
-        # Activation rates
-        self.p_act = float(p_act)
-        # Path fractions
-        self.frac_memory = float(frac_memory)
-        self.S_C_Ratio = float(S_C_Ratio)
-        # Pathway rates
-        self.p_SC = float(p_SC)
-        self.p_CR = float(p_CR)
-        # Proliferate rates
-        self.r_N = float(r_N)
-        self.r_S = float(r_S)
-        self.r_C = float(r_C)
-        self.r_R = float(r_R)
-        # Death rates
-        self.d_N = float(d_N)
-        self.d_S = float(d_S)
-        self.d_C = float(d_C)
-        self.d_R = float(d_R)
-        # Initial population
-        self.initial_population = int(initial_population)
-        self.global_factor = 'Undefined'
+        self.mu_N = mu_N
+        self.mu_TSCM = mu_TSCM
+        self.mu_TCM = mu_TCM
+        self.mu_TEM = mu_TEM
+        self.mu_TEMRA = mu_TEMRA
+        self.mu_MPEC = mu_MPEC
+        self.mu_SLEC = mu_SLEC
+        self.f_TSCM = f_TSCM
+        self.f_TCM = f_TCM
+        self.f_TEM = f_TEM
+        self.f_TEMRA = f_TEMRA
+        self.t_peak = t_peak
+        self.sigma = sigma
+        self.q = q
+        self.alpha_peak = alpha_peak
+        self.b_MPEC = b_MPEC
+        self.b_SLEC = b_MPEC
+        self.K_mem = K_mem
+        self.S_CD4 = S_CD4
 
         # create CD4Cells
-        for i in range(self.initial_population):
-           CD4Cell(self, cell_type = 'N')
+        for _ in range(self.S_CD4):
+           CD4Cell(self, cell_type = 'Naive')
 
 
         # initiate data collector
         self.datacollector = mesa.DataCollector(
             model_reporters={
-                "%N": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'N') / self.initial_population)*100,
-                "%S": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'S') / self.initial_population)*100,
-                "%C": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'C') / self.initial_population)*100,
-                "%R": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'R') / self.initial_population)*100,
-                "%Total_memory": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type in ['S', 'C', 'R']) / self.initial_population)*100,
+                "%N": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'Naive') / self.S_CD4)*100,
+                "%S": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'TSCM') / self.S_CD4)*100,
+                "%C": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'TCM') / self.S_CD4)*100,
+                "%R": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type == 'TEMRA') / self.S_CD4)*100,
+                "%Total_memory": lambda m: (sum(1 for agent in m.agents_by_type[CD4Cell] if agent.cell_type in ['TSCM', 'TCM', 'TEMRA']) / self.S_CD4)*100,
                 "Total_Live": lambda m: len(m.agents)
             }
 )
@@ -152,3 +133,8 @@ class Immunology_Model(mesa.Model):
             self.agents_by_type[agent_class].shuffle_do("step")
         # collect model level data
         self.datacollector.collect(self)
+
+if __name__=="__main__":
+    run = Immunology_Model()
+    for _ in range(100):
+        run.step()
